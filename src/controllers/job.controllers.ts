59fcf252c8fb302ee6e07fcc5b7597997df36441
raazymuhd1 <muhammadrazi51@gmail.com>
2026-01-23 16:13:53 +0700

    create and test the createJob endpoint

--- src/controllers/job.controllers.ts
+++ src/controllers/job.controllers.ts
@@ -1,7 +1,50 @@
 import { prisma } from "../lib/prisma";
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 
-const createJob = async() => {
+const createJob = async(req: Request, res: Response) => {
+    const { companyName, companyPicture, role, requirements, responsibilities, description, detail } = req.body;
+    const { authorId } = req.query;
 
+    if(!(companyName || companyPicture || role || requirements || responsibilities || description || detail)) {
+         console.log(`missing required fields`)
+         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ data: null, msg: "missing required fields" })
+          return;
+    }
+
+    const author = await prisma.user.findFirst({
+        where: { id: Number(authorId) },
+        select: {
+            id: true,
+            username: true,
+            email: true,
+            picture: true
+        }
+    })
+
+    if(!author) {
+          console.log(`unrecognized author`)
+         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ data: null, msg: "unrecognized author" })
+          return;
+    }
+
+    const job = await prisma.job.create({
+        data: {
+            companyName,
+            companyPicture,
+            role,
+            requirements,
+            responsibilities,
+            description,
+            createdAt: new Date(),
+            detail,
+            authorId: Number(authorId),
+        }
+    })
+
+    console.log(`createdAt`, new Date())
+    res.status(StatusCodes.OK).json({ data: { job, author }, msg: "new job created" })
+    return;
 }
 
 const getAllJobs = async() => {
--- src/routes/jobs.routes.ts
+++ src/routes/jobs.routes.ts
@@ -3,3 +3,11 @@ import asyncHandler from "express-async-handler"
 import { createJob, getAllJobs, getJob } from "../controllers/job.controllers"
 
 const router = express.Router()
+
+// POST
+router.post("/create-job", asyncHandler(createJob))
+
+
+// GET
+router.get("/get-all-jobs", asyncHandler(getAllJobs))
+router.get("/get-job/:jobId", asyncHandler(getJob))
